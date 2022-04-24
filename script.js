bf6575f18f7652f907a001811f68dfe569b4df1a
HARMOUSH BARAA <94456385+Harmoush17@users.noreply.github.com>
2022-04-24 08:33:33 +0200

    onload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-let c = document.getElementById()
+let welKom = document.getElementById('welkom');
+window.onload = function(){
+    let value = onload;
+    welKom.style.display = 'block';
+    welKom.style.transformDelay = '1s';
+}
