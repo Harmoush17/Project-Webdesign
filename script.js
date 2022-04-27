8fb6b8b3e118ca77d921709a9878172edbf4c69d
HARMOUSH BARAA <94456385+Harmoush17@users.noreply.github.com>
2022-04-27 09:07:00 +0200

    menu

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let welKom = document.getElementById('welkom');
-window.onload = function(){
-    let value = onload;
-    welKom.style.display = 'block';
-    welKom.style.transformDelay = '1s';
-}
+// let welKom = document.getElementById('welkom');
+// window.onload = function(){
+//     let value = onload;
+//     welKom.style.display = 'block';
+//     welKom.style.transformDelay = '1s';
+// }
+
+
+// to make the navigation appear
+const mobileMenu = document.querySelector('#mobile-menu')
+const navUl = document.querySelector('.nav-items')
+mobileMenu.addEventListener("click", function(){
+    navUl.classList.toggle("appear")
+})
