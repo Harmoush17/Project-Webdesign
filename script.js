1282ff2e807bedde5a098d95c001af8ee2f91537
harmoush <harmoush.baraa@gmail.com>
2022-04-28 12:13:05 +0200

    menu.responsive

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-// let welKom = document.getElementById('welkom');
-// window.onload = function(){
-//     let value = onload;
-//     welKom.style.display = 'block';
-//     welKom.style.transformDelay = '1s';
-// }
-
-
 // to make the navigation appear
 const mobileMenu = document.querySelector('#mobile-menu')
 const navUl = document.querySelector('.nav-items')
 mobileMenu.addEventListener("click", function(){
     navUl.classList.toggle("appear")
 })
+
+// document.addEventListener('mouseenter', function())
