e82eb0c1d0eebf89a3fd28f14aeccb18745d0729
HARMOUSH BARAA <94456385+Harmoush17@users.noreply.github.com>
2022-04-30 07:19:52 +0200

    photo

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // to make the navigation appear
 const mobileMenu = document.querySelector('#mobile-menu')
 const navUl = document.querySelector('.nav-items')
+const navLinks = document.querySelectorAll('.nav-links');
+
 mobileMenu.addEventListener("click", function(){
     navUl.classList.toggle("appear")
 })
+
+navLinks.forEach((navLink) =>{
+    navLink.addEventListener('click', () => {
+        navUl.classList.toggle("appear")
+    })
+})
+
 
 // document.addEventListener('mouseenter', function())
